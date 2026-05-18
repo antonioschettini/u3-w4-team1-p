8d46ded18547cfdf526eb4a39cbf68923f462378
Andrea Saderi <andrea.saderi16@gmail.com>
2026-05-18 13:40:58 +0200

    container dentro profile

--- src/components/pages/profile/Profile.jsx
+++ src/components/pages/profile/Profile.jsx
@@ -1,15 +1,20 @@
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import RightLanguageAndUrl from "./RightLanguageAndUrl";
 import WhoVisited from "./WhoVisited";
 
 const Profile = () => {
   return (
-    <Row>
-      <Col xs={3} className="flex-column">
-        <RightLanguageAndUrl />
-        <WhoVisited />
-      </Col>
-    </Row>
+    <Container>
+      <Row>
+        <Col xs={9}>
+          <p>Ciao</p>
+        </Col>
+        <Col xs={3} className="d-flex flex-column">
+          <RightLanguageAndUrl />
+          <WhoVisited />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
