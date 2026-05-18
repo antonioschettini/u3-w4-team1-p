1d46be282f8dbdc60ce214546e288c306c100d17
Andrea Saderi <andrea.saderi16@gmail.com>
2026-05-18 14:35:52 +0200

    fixes right part spacings

--- src/components/pages/profile/PeopleYouMayKnow.jsx
+++ src/components/pages/profile/PeopleYouMayKnow.jsx
@@ -1,8 +1,9 @@
+import { ArrowRightShort } from "react-bootstrap-icons";
 import PeopleCard from "./PeopleCard";
 
 const PeopleYouMayKnow = () => {
   return (
-    <div className="d-none d-sm-flex flex-column border border-1 border-secondary-subtle rounded-2 p-3 my-2 bg-white">
+    <div className="d-none d-md-flex flex-column border border-1 border-secondary-subtle rounded-2 p-3 my-2 bg-white">
       <div className="d-flex flex-column">
         <div className="d-flex justify-content-between align-items-center">
           <p className="fw-semibold m-0">Persone che potresti conoscere</p>
@@ -16,6 +17,9 @@ const PeopleYouMayKnow = () => {
         <PeopleCard />
         <PeopleCard />
         <PeopleCard />
+      </div>
+      <div className="text-center text-secondary-emphasis fw-semibold border-top border-tertiary border-1 pt-2">
+        Mostra tutto <ArrowRightShort />
       </div>
     </div>
   );
