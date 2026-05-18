315c90f4892b051a7e05ff6ab4d85bdf4f8e9276
Andrea Saderi <andrea.saderi16@gmail.com>
2026-05-18 12:14:09 +0200

    deletes Col system from rightLanguage

--- src/components/pages/profile/RightLanguageAndUrl.jsx
+++ src/components/pages/profile/RightLanguageAndUrl.jsx
@@ -1,12 +1,8 @@
-import { Col } from "react-bootstrap";
 import { Pencil } from "react-bootstrap-icons";
 
 const RightLanguageAndUrl = () => {
   return (
-    <Col
-      xs={3}
-      className="d-none d-sm-flex flex-column border border-1 border-secondary-subtle rounded-2 p-3 h-auto"
-    >
+    <div className="d-none d-sm-flex flex-column border border-1 border-secondary-subtle rounded-2 p-3 h-auto">
       <div className="d-flex flex-column border-bottom border-1 border-tertiary">
         <div className="d-flex justify-content-between align-items-center">
           <p className="fw-semibold m-0">Lingua del profilo</p>
@@ -25,7 +21,7 @@ const RightLanguageAndUrl = () => {
           www.linkedin.com/in/andrea-saderi-9b0aa5152
         </p>
       </div>
-    </Col>
+    </div>
   );
 };
 
