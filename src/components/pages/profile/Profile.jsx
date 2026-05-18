2f0a92f1f4c5335ea761eb618d1021410db0ff7f
antonio.schettini <antonio.schettini93@gmail.com>
2026-05-18 13:34:22 +0200

    .

--- src/components/pages/profile/Profile.jsx
+++ src/components/pages/profile/Profile.jsx
@@ -1,10 +1,19 @@
 import { Container, Row } from "react-bootstrap";
 import RightLanguageAndUrl from "./RightLanguageAndUrl";
+import ProfileHero from "./ProfileHero";
+import Analisi from "./Analisi";
+import Attività from "./Attività";
+import ConsigliatoPerTe from "./ConsigliatoPerTe";
 
 const Profile = () => {
   return (
     <>
       <Container>
+        <ProfileHero />
+        <ConsigliatoPerTe />
+        <Analisi />
+        <Attività />
+
         <Row>
           <RightLanguageAndUrl />
         </Row>
