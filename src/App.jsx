5e752939781a89384dfeccacb9eaa82899c3b0c8
antonio.schettini <antonio.schettini93@gmail.com>
2026-05-19 12:14:04 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import Home from "./components/pages/home/Home"
-import Profile from "./components/pages/profile/Profile"
-import MyFooter from "./components/MyFooter"
-import MyNavbar from "./components/MyNavbar"
-import { BrowserRouter, Route, Routes } from "react-router"
-import Messaggistica from "./components/Messaggistica"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/pages/home/Home";
+import Profile from "./components/pages/profile/Profile";
+import MyFooter from "./components/MyFooter";
+import MyNavbar from "./components/MyNavbar";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Messaggistica from "./components/Messaggistica";
 
 function App() {
   return (
@@ -12,14 +12,14 @@ function App() {
       <div className="d-flex flex-column">
         <MyNavbar />{" "}
         <Routes>
-           <Route path="/" element={<Home />} />
-          <Route path="/" element={<Profile />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
         <Messaggistica />
         <MyFooter />
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
