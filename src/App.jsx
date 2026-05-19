683f0dbc31f943dd8711bb5245ebeb9f0f236140
antonio.schettini <antonio.schettini93@gmail.com>
2026-05-19 13:01:45 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,3 @@
-<<<<<<< Updated upstream
-import "bootstrap/dist/css/bootstrap.min.css"
-import Profile from "./components/pages/profile/Profile"
-import MyFooter from "./components/MyFooter"
-import MyNavbar from "./components/MyNavbar"
-import { BrowserRouter, Route, Routes } from "react-router"
-import Messaggistica from "./components/Messaggistica"
-=======
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/pages/home/Home";
 import Profile from "./components/pages/profile/Profile";
@@ -14,7 +6,6 @@ import MyNavbar from "./components/MyNavbar";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Messaggistica from "./components/Messaggistica";
 import NotFound from "./components/pages/notfound/NotFound";
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -22,13 +13,9 @@ function App() {
       <div className="d-flex flex-column">
         <MyNavbar />{" "}
         <Routes>
-<<<<<<< Updated upstream
-          <Route path="/" element={<Profile />} />
-=======
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<NotFound />} />
->>>>>>> Stashed changes
         </Routes>
         <Messaggistica />
         <MyFooter />
