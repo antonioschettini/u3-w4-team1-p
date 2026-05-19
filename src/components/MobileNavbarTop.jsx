b26c92dd56e395b18a5caf73e32436422b8f2ed5
nicolorsillo <nicolorsillo@hotmail.it>
2026-05-19 15:07:50 +0200

    fixed offcanvas

--- src/components/MobileNavbarTop.jsx
+++ src/components/MobileNavbarTop.jsx
@@ -1,4 +1,4 @@
-import { ChatDotsFill, GearFill, Search } from "react-bootstrap-icons";
+import { ChatDotsFill, GearFill, Search } from "react-bootstrap-icons"
 import {
   Form,
   Container,
@@ -7,17 +7,17 @@ import {
   Image,
   Button,
   Offcanvas,
-} from "react-bootstrap";
-import { Link } from "react-router";
-import { useSelector } from "react-redux";
-import { useState } from "react";
+} from "react-bootstrap"
+import { Link } from "react-router"
+import { useSelector } from "react-redux"
+import { useState } from "react"
 
 function MobileNavbarTop() {
-  const profilo = useSelector((state) => state.profilo.mioProfilo);
-  const [show, setShow] = useState(false);
+  const profilo = useSelector((state) => state.profilo.mioProfilo)
+  const [show, setShow] = useState(false)
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
 
   return (
     <Navbar expand="lg" className="bg-white py-1">
@@ -33,7 +33,11 @@ function MobileNavbarTop() {
         <Offcanvas show={show} onHide={handleClose} style={{ width: "70vw" }}>
           <Offcanvas.Header className=" border-bottom">
             <Offcanvas.Title>
-              <Link to="/profile" className=" text-decoration-none">
+              <Link
+                to="/profile"
+                className=" text-decoration-none"
+                onClick={handleClose}
+              >
                 <Image
                   src={profilo?.image || "https://placehold.co/150"}
                   roundedCircle
@@ -93,7 +97,7 @@ function MobileNavbarTop() {
         </Link>
       </Container>
     </Navbar>
-  );
+  )
 }
 
-export default MobileNavbarTop;
+export default MobileNavbarTop
