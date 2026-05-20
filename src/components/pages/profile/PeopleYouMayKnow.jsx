4d232738875b8e0c303b8efb683954da2d7358d9
Andrea Saderi <andrea.saderi16@gmail.com>
2026-05-20 10:38:24 +0200

    Update PeopleYouMayKnow.jsx

--- src/components/pages/profile/PeopleYouMayKnow.jsx
+++ src/components/pages/profile/PeopleYouMayKnow.jsx
@@ -16,7 +16,7 @@ const PeopleYouMayKnow = () => {
   }, []);
 
   return (
-    <div className="d-none d-md-flex flex-column border border-1 border-secondary-subtle rounded-2 p-3 my-2 bg-white shadow-sm">
+    <div className="d-none d-md-flex flex-column border border-1 border-secondary-subtle rounded-2 p-3 mb-2 bg-white shadow-sm">
       <div className="d-flex flex-column">
         <div className="d-flex justify-content-between align-items-center">
           <p className="fw-semibold m-0">Persone che potresti conoscere</p>
