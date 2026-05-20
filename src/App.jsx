91f95ae0a2e1b31eff923fe784cba575ac098ae9
nicolorsillo <nicolorsillo@hotmail.it>
2026-05-20 11:56:14 +0200

    Added search peaple

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./components/pages/home/Home";
-import Profile from "./components/pages/profile/Profile";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Home from "./components/pages/home/Home"
+import Profile from "./components/pages/profile/Profile"
 
-import MyNavbar from "./components/MyNavbar";
-import { BrowserRouter, Route, Routes } from "react-router";
-import MyFooter from "./components/MyFooter";
-import Messaggistica from "./components/Messaggistica";
-import NotFound from "./components/pages/notfound/NotFound";
+import MyNavbar from "./components/navbar/MyNavbar"
+import { BrowserRouter, Route, Routes } from "react-router"
+import MyFooter from "./components/MyFooter"
+import Messaggistica from "./components/Messaggistica"
+import NotFound from "./components/pages/notfound/NotFound"
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
         <MyFooter />
       </div>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/navbar/PeopleLinkCard.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router"
+import { Image, NavDropdown } from "react-bootstrap"
+
+const PeolpleLinkCard = (props) => {
+  const { image, name, surname, title, _id } = props.profile
+  const { resetSearch } = props
+  return (
+    <>
+      <NavDropdown.Item as={"div"} className="m-0 p-0">
+        <Link
+          className="d-flex align-items-center my-1 text-decoration-none"
+          to={
+            _id === "6a0afbe906bbe90015dee589" ? "/profile" : `/profile/${_id}`
+          }
+          onClick={() => resetSearch("")}
+        >
+          <Image
+            src={image}
+            roundedCircle
+            width={"40px"}
+            height={"40px"}
+            className="me-2"
+          />
+          <h6 className="text-nowrap fw-bold mb-0 text-black">
+            {name} {surname}
+          </h6>
+          {title ? (
+            <>
+              <small className="text-muted mx-2">•</small>
+              <small className="text-muted text-truncate">{title}</small>
+            </>
+          ) : null}
+        </Link>
+      </NavDropdown.Item>
+    </>
+  )
+}
+export default PeolpleLinkCard
